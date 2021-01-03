6d700d090f3b43e57804690a3b2b81bf5b1da52e
Nandini Sharma <46066660+idiosyncratic4@users.noreply.github.com>
2021-01-03 16:17:35 +0530

    system changes

---
+++ server/env/environment.js
@@ -0,0 +1,9 @@
+const cosmosPort = 10255; // replace with your port
+const dbName = 'codecapture-demo';
+const key = 'nojyf5xqUZ6jt5kP6Iel9lgOMacUpyY3NEu3HExKSGYT4FS1MCv1fXBqamYExED2bsXBQNW6BdeuWn2lHuXClg==';
+
+module.exports = {
+  cosmosPort,
+  dbName,
+  key
+};
